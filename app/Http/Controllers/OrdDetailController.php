2f407928ccad754f5f9b633f1a1a7f4a4b47893c
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-12-19 22:32:06 +0800

    ord_detail 建立資料表欄位

---
+++ app/Http/Controllers/OrdDetailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ord_detail;
+use App\Http\Requests\StoreOrd_detailRequest;
+use App\Http\Requests\UpdateOrd_detailRequest;
+
+class OrdDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrd_detailRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrd_detailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ord_detail  $ord_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ord_detail $ord_detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ord_detail  $ord_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ord_detail $ord_detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrd_detailRequest  $request
+     * @param  \App\Models\Ord_detail  $ord_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOrd_detailRequest $request, Ord_detail $ord_detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ord_detail  $ord_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ord_detail $ord_detail)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_12_19_142836_create_ord_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ord_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('orders_id');
+            $table->foreign('orders_id')->references('id')->on('orders');  #訂單編號
+
+            $table->unsignedBigInteger('products_id');
+            $table->foreign('products_id')->references('id')->on('products');  #產品編號
+
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');  #會員編號
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ord_details');
+    }
+}
