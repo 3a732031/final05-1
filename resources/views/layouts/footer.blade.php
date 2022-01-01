61aab28c2b85e11460b1421dfbb5630fec0bfee9
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2022-01-01 16:02:48 +0800

    建立navigation、foter layouts並引用

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Footer-->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Bottle 建議使用Chorme瀏覽</p>
+        <p class="m-0 text-center text-white">Copyright &copy; 2021-2022</p>
+        <p class="m-0 text-center text-white">服務時間：周一至周五09:00-17:00</p>
+    </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/barscripts.js"></script>
