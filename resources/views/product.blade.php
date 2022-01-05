86c5dab52d90fd7962d303b787e08af83e53fd2b
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2022-01-05 11:04:56 +0800

    修改首頁及產品頁樣式

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <!-- Section-->
 
-    <section class="py-5">
+    <section class="py-5 bg-3">
 
         <div class="container px-4 px-lg-5 mt-5">
 
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 @foreach ($product as $products)
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card h-100  bg-4">
                             <!-- Product image-->
                             <img class="card-img-top" src="images/{{$products->image}}" alt="..." />
                             <!-- Product details-->
@@ -24,7 +24,7 @@
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">加入購物車</a></div>
+                                <div class="text-center"><a class="btn btn-outline-success mt-auto" href="#">加入購物車</a></div>
                             </div>
                         </div>
                     </div>
